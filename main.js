eab95dd00026c98915b5f765fed79e1cfc0f157d
mdacht777 <mdacht777@gmail.com>
2020-10-25 20:30:04 -0600

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.getElementById('modal')
+
 
 // Your JavaScript code goes here!
 
+let articleHearts = document.querySelectorAll(".like-glyph");
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
+function likeCallback(e) {
+      // modal.className = "hidden"
+  let heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      // STEP 2: Uncomment the next 3 lines.
+      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
+      // mimic Pillar 3 (Server Communication) to only update the screen if the
+      // sending of information to the server succeeds.
+      // alert("You notified the server!");
+      // alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      // alert("Something went wrong!");
+      modal.className = ""
+      modal.textContent = error
+      setTimeout(function(){ modal.className = "hidden"; }, 3000);
+    });
+}
 
 
 //------------------------------------------------------------------------------
